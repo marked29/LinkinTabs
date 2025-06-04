2d3de1d4949a9096f0e112c55ff1f61226d52131
Mark Misra <mark.mi@nextcode.tech>
2025-06-04 21:16:02 +0300

    feat: add open bulk functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 import { Card, CardContent } from './components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
@@ -7,6 +7,15 @@ import { Textarea } from '@/components/ui/textarea';
 function App() {
   const [tabs, setTabs] = useState<any[]>([]);
   const [bulkLinks, setBulkLinks] = useState<string>('');
+
+  useEffect(() => {
+    if (!chrome || !chrome.tabs) {
+      console.error('Chrome tabs API is not available');
+      return;
+    }
+
+    fetchAllTabs();
+  }, []);
 
   const fetchAllTabs = async () => {
     const tabs = await chrome.tabs.query({});
@@ -62,13 +71,13 @@ function App() {
       <main className="w-[300px] p-4 space-y-4">
         <h1 className="text-xl font-bold text-center">Linkin Tabs</h1>
         <Tabs defaultValue="opened">
-          <TabsList>
+          <TabsList className="w-full mx-auto flex justify-center">
             <TabsTrigger value="opened">Current tabs</TabsTrigger>
             <TabsTrigger value="closed">Open bulk</TabsTrigger>
           </TabsList>
           <TabsContent value="opened">
             <Card>
-              <CardContent className="space-y-2 py-2">
+              <CardContent className="space-y-2 py-1">
                 <ul>
                   {tabs && tabs.length > 0 ? (
                     tabs.map((link, idx) => (
@@ -79,24 +88,26 @@ function App() {
                       </li>
                     ))
                   ) : (
-                    <li className="text-sm text-gray-500">No tabs opened</li>
+                    <div className="flex flex-col items-center justify-center space-y-2">
+                      <li className="text-sm text-gray-500">No tabs opened</li>
+                      <li>
+                        <Button onClick={fetchAllTabs}>Get all tabs</Button>
+                      </li>
+                    </div>
                   )}
                 </ul>
               </CardContent>
             </Card>
 
             <div className="flex gap-2 mt-2">
-              <Button className="flex-1" onClick={fetchAllTabs}>
-                Get all tabs
-              </Button>
               <Button className="flex-1" onClick={handleCopyAllLinks}>
-                Add All links
+                Copy all links
               </Button>
             </div>
           </TabsContent>
           <TabsContent value="closed">
             <Card>
-              <CardContent className="space-y-2 py-2">
+              <CardContent>
                 <Textarea placeholder="Type in your links to open" value={bulkLinks} onChange={(e) => setBulkLinks(e.target.value)} />
               </CardContent>
             </Card>
